3a959ca9178454c7a368a27b6fceefcfa575e0bf
Byron Lopez <merlin2010@gmail.com>
2019-10-05 11:19:05 -0600

    Adding people pagination.

--- src/queries/people.js
+++ src/queries/people.js
@@ -1,25 +1,50 @@
 const db = require('../models');
 const sequelize = require('sequelize');
 const Op = sequelize.Op;
+const { paginate } = require('./extensions');
 
 module.exports = {
+  allPeoplePaginated: async (parent, { name, offset, limit }) => {
+    if (!offset || !limit) {
+      throw new Error('Missing offset and/or limit fields');      
+    }
+
+    const where = name ? {
+      name: {
+        [Op.like]: `%${name.trim()}%`
+      }
+    } : {};
+
+    const result = await db.people.findAll({
+      where,
+      ...paginate({ page: offset, pageSize: limit })
+    });
+
+    const totalCount = await db.people.count({
+      where
+    });
+
+    return {
+      people: result,
+      totalCount,
+      limit,
+      offset
+    };
+  },
   allPeople: (parent, args) => {
     try {
-      console.log('GETTING PEOPLE');
       return db.people.findAll();
     } catch (err) {
       console.log('ERROR GETTING PEOPLE', err);
     }
   },
   findPeopleByName: (parent, { name }) => {
-    const result = db.people.findAll({
+    return db.people.findAll({
       where: {
         name: {
           [Op.like]: `%${name.trim()}%`
         }
       }
     });
-
-    return result;
   }
 };
